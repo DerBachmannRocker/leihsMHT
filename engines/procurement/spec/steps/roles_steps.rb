575cef2d0186578056d4ea02503005219e39d5bc
Franco Sellitto <franco.sellitto@zhdk.ch>
2016-05-17 12:33:15 +0200

    adapting tests

--- engines/procurement/spec/steps/roles_steps.rb
+++ engines/procurement/spec/steps/roles_steps.rb
@@ -56,7 +56,7 @@ steps_for :roles do
                          organization: \
                          FactoryGirl.create(:procurement_organization))
       prepare_request requester: requester
-      @group.inspectors << @current_user
+      @category.inspectors << @current_user
       go_to_request user: requester
 
       if boolean
@@ -135,12 +135,12 @@ steps_for :roles do
     expect(page).to have_content _('Request moved')
   end
 
-  step 'I can move requests to other groups' do
-    new_group = FactoryGirl.create(:procurement_group)
+  step 'I can move requests to other categories' do
+    new_category = FactoryGirl.create :procurement_category, :as_leaf
     prepare_request
     go_to_request
     find(".row[data-request_id='#{@request.id}'] button .fa-gear").click
-    find("a[href*='move?to_category_id=#{new_group.id}']").click
+    find("a[href*='move?to_category_id=#{new_category.id}']").click
     expect(page).to have_content _('Request moved')
   end
 
@@ -151,22 +151,22 @@ steps_for :roles do
                        organization: \
                          FactoryGirl.create(:procurement_organization))
     prepare_request requester: requester
-    expect(@group.inspectors).not_to include @current_user
+    expect(@category.inspectors).not_to include @current_user
     go_to_request user: requester
     expect(page).not_to have_selector ".row[data-request_id='#{@request.id}']"
   end
 
-  step 'I can not inspect requests of my own group' do
-    requester = FactoryGirl.create(:user)
-    FactoryGirl.create(:procurement_access,
-                       user: requester,
-                       organization: \
-                         FactoryGirl.create(:procurement_organization))
-    prepare_request requester: requester
-    @group.inspectors << @current_user
-    go_to_request user: requester
-    step 'I can not inspect certain fields'
-  end
+  # step 'I can not inspect requests of my own group' do
+  #   requester = FactoryGirl.create(:user)
+  #   FactoryGirl.create(:procurement_access,
+  #                      user: requester,
+  #                      organization: \
+  #                        FactoryGirl.create(:procurement_organization))
+  #   prepare_request requester: requester
+  #   @group.inspectors << @current_user
+  #   go_to_request user: requester
+  #   step 'I can not inspect certain fields'
+  # end
 
   step 'I can not add requester' do
     expect(find('.navbar').text).not_to match _('Admin')
@@ -192,7 +192,7 @@ steps_for :roles do
     expect(page).to have_content _('You are not authorized for this action.')
   end
 
-  step 'I can not add groups' do
+  step 'I can not add categories' do
     expect(find('.navbar').text).not_to match _('Admin')
     visit procurement.categories_path
     expect(page).to have_content _('You are not authorized for this action.')
@@ -204,12 +204,12 @@ steps_for :roles do
     expect(page).to have_content _('You are not authorized for this action.')
   end
 
-  step 'I can not manage templates' do
-    group = FactoryGirl.create(:procurement_group)
-    expect(find('.navbar').text).not_to match _('Templates')
-    visit procurement.group_templates_path(group_id: group.id)
-    expect(page).to have_content _('You are not authorized for this action.')
-  end
+  # step 'I can not manage templates' do
+  #   group = FactoryGirl.create(:procurement_group)
+  #   expect(find('.navbar').text).not_to match _('Templates')
+  #   visit procurement.group_templates_path(group_id: group.id)
+  #   expect(page).to have_content _('You are not authorized for this action.')
+  # end
 
   step 'I can not create requests for another person' do
     @budget_period = FactoryGirl.create(:procurement_budget_period)
@@ -238,34 +238,34 @@ steps_for :roles do
   step 'I can not see budget limits' do
     prepare_request
     FactoryGirl.create(:procurement_budget_limit,
-                       group: @group,
+                       category: @category,
                        budget_period: @budget_period)
     go_to_request
     expect(page).not_to have_selector '.budget_limit'
   end
 
-  step 'I can edit a request of group where I am an inspector' do
+  step 'I can edit a request of a category where I am an inspector' do
     requester = FactoryGirl.create(:user)
     FactoryGirl.create(:procurement_access,
                        user: requester,
                        organization: \
                          FactoryGirl.create(:procurement_organization))
     prepare_request requester: requester
-    @group.inspectors << @current_user
+    @category.inspectors << @current_user
     go_to_request user: requester
     find("[name='requests[#{@request.id}][article_name]']").set Faker::Lorem.word
     find('button', text: _('Save'), match: :first).click
     expect(page).to have_content _('Saved')
   end
 
-  step 'I can delete a request of group where I am an inspector' do
+  step 'I can delete a request of a category where I am an inspector' do
     requester = FactoryGirl.create(:user)
     FactoryGirl.create(:procurement_access,
                        user: requester,
                        organization: \
                          FactoryGirl.create(:procurement_organization))
     prepare_request requester: requester
-    @group.inspectors << @current_user
+    @category.inspectors << @current_user
     go_to_request user: requester
     find(".row[data-request_id='#{@request.id}'] button .fa-gear").click
     accept_alert do
@@ -274,14 +274,14 @@ steps_for :roles do
     expect(page).to have_content _('Deleted')
   end
 
-  step 'I can move requests of my own group to other budget periods' do
+  step 'I can move requests of my own category to other budget periods' do
     requester = FactoryGirl.create(:user)
     FactoryGirl.create(:procurement_access,
                        user: requester,
                        organization: \
                          FactoryGirl.create(:procurement_organization))
     prepare_request requester: requester
-    @group.inspectors << @current_user
+    @category.inspectors << @current_user
     new_budget_period = FactoryGirl.create(:procurement_budget_period)
     go_to_request user: requester
     find(".row[data-request_id='#{@request.id}'] button .fa-gear").click
@@ -290,22 +290,22 @@ steps_for :roles do
     expect(page).to have_content _('Request moved')
   end
 
-  step 'I can move requests of my own group to other groups' do
+  step 'I can move requests of my own category to other categories' do
     requester = FactoryGirl.create(:user)
     FactoryGirl.create(:procurement_access,
                        user: requester,
                        organization: \
                          FactoryGirl.create(:procurement_organization))
     prepare_request requester: requester
-    @group.inspectors << @current_user
-    new_group = FactoryGirl.create(:procurement_group)
+    @category.inspectors << @current_user
+    new_category = FactoryGirl.create(:procurement_category)
     go_to_request user: requester
     find(".row[data-request_id='#{@request.id}'] button .fa-gear").click
-    find("a[href*='move?to_category_id=#{new_group.id}']").click
+    find("a[href*='move?to_category_id=#{new_category.id}']").click
     expect(page).to have_content _('Request moved')
   end
 
-  step 'I can create requests for my group for another person' do
+  step 'I can create requests for my category for another person' do
     @category = FactoryGirl.create(:procurement_category, :as_leaf)
     @category.inspectors << @current_user
     @budget_period = FactoryGirl.create(:procurement_budget_period)
@@ -318,7 +318,7 @@ steps_for :roles do
     expect(page).not_to have_content _('You are not authorized for this action.')
   end
 
-  step 'I can manage templates of my group' do
+  step 'I can manage templates for categories I am inspector' do
     @category = FactoryGirl.create(:procurement_category, :as_leaf)
     @category.inspectors << @current_user
     step 'I navigate to procurement'
